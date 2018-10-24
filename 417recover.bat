725404eafdf6eb3f6e0d31c6f2aeb0a9bd1fc88f
Neboer <43609792+Neboer@users.noreply.github.com>
2018-10-24 14:40:54 +0800

    Add files via upload

---
+++ 417recover.bat
@@ -0,0 +1,17 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+@echo on
+netsh interface ip set address "WLAN" static 58.154.164.52 255.255.0.0 58.154.1.1
+netsh interface ip set address name="WLAN" source=dhcp
+
+pause
